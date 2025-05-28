46cf1de05252920977add24619da52f7db6d9397
victor <victor.edu22@gmail.com>
2025-05-28 00:45:39 -0400

    add conbeccion a mongo

--- estadoRed.py
+++ estadoRed.py
@@ -1,15 +1,11 @@
 from playwright.sync_api import sync_playwright, TimeoutError
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
+from connection import get_apartment, update_apartment
 
 from dotenv import load_dotenv
 import os
-import json
 
-load_dotenv()  # Carga las variables del archivo .env
-
-slack_token = os.getenv('SLACK_TOKEN')
-client = WebClient(token=slack_token)
 
 def enviar_mensaje_a_slack(mensaje):
     try:
@@ -22,61 +18,64 @@ def enviar_mensaje_a_slack(mensaje):
         print("Error al enviar mensaje:", e.response["error"])
 
 
-# Abrir y leer el archivo
-with open('departamentos.txt', 'r', encoding='utf-8') as f:
-    departamentos = json.load(f)
+def main():
+    load_dotenv()  # Cargar variables del .env
 
+    slack_token = os.getenv("SLACK_TOKEN")
+    global client
+    client = WebClient(token=slack_token)
 
-with sync_playwright() as p:
-    for depto in departamentos:
-        navegador = p.chromium.launch(headless=False)
-        contexto = navegador.new_context(ignore_https_errors=True)
-        pagina = contexto.new_page()
+    apartamentos = get_apartment()
 
-        depto["intentos"] = 0
-        with open('departamentos.txt', 'w', encoding='utf-8') as f:
-            json.dump(departamentos, f, indent=4, ensure_ascii=False)
+    with sync_playwright() as p:
+        for depto in apartamentos:
+            navegador = p.chromium.launch(headless=False)
+            contexto = navegador.new_context(ignore_https_errors=True)
+            pagina = contexto.new_page()
 
-        try:
+            # Actualiza intentos a 0 (por ejemplo)
+            resultado = update_apartment(depto["id"], {"attemps": 0})
+            
+
             try:
-                pagina.goto(depto["url"])
+                try:
+                    pagina.goto(depto["url"])
+                except Exception as e:
+                    mensaje = f"❌ No se pudo conectar a {depto['name']} ({depto['url']}): {e}"
+                    enviar_mensaje_a_slack(mensaje)
+                    navegador.close()
+                    continue
 
-            except Exception as e:
-                mensaje = f"❌ No se pudo conectar a {depto['nombre']} ({depto['url']}): {e}"
+                pagina.locator("input[type='text']").nth(0).fill(depto["user"])
+                pagina.locator("input[type='password']").nth(0).fill(depto["password"])
+
+                try:
+                    pagina.locator("text=Acceder").nth(1).click()
+                except Exception as e:
+                    mensaje = f"No se pudo hacer clic en Acceder en {depto['name']}: {e}"
+                    enviar_mensaje_a_slack(mensaje)
+                    navegador.close()
+                    continue
+
+                try:
+                    pagina.wait_for_selector("#lan-info-ip", timeout=5000)
+                    if pagina.locator("#lan-info-ip").is_visible():
+                        ip_texto = pagina.locator("#lan-info-ip pre").inner_text()
+                        mensaje = f"IP encontrada en {depto['name']}: `{ip_texto}`"
+                    else:
+                        mensaje = f"Se ingresó, pero no se encontró la IP en {depto['name']}."
+                except TimeoutError:
+                    if pagina.url.endswith("/login") or "login" in pagina.title().lower():
+                        mensaje = f"Credenciales incorrectas para {depto['name']}."
+                    else:
+                        mensaje = f"No se encontró la IP. Timeout en {depto['name']}."
+
                 enviar_mensaje_a_slack(mensaje)
+                pagina.wait_for_timeout(3000)
+
+            finally:
                 navegador.close()
-                continue  # Pasar al siguiente departamento
-
-            # Llenar usuario y contraseña dinámicamente
-            pagina.locator("input[type='text']").nth(0).fill(depto["usuario"])
-            pagina.locator("input[type='password']").nth(0).fill(depto["contrasena"])
-
-            # Intentar login
-            try:
-                pagina.locator("text=Acceder").nth(1).click()
-            except Exception as e:
-                mensaje = f"No se pudo hacer clic en Acceder en {depto['nombre']}: {e}"
-                enviar_mensaje_a_slack(mensaje)
-                navegador.close()
-                continue  # ✅ Ahora sí dentro del for
-
-            # Esperar IP
-            try:
-                pagina.wait_for_selector("#lan-info-ip", timeout=5000)
-                if pagina.locator("#lan-info-ip").is_visible():
-                    ip_texto = pagina.locator("#lan-info-ip pre").inner_text()
-                    mensaje = f"IP encontrada en {depto['nombre']}: `{ip_texto}`"
-                else:
-                    mensaje = f"Se ingresó, pero no se encontró la IP en {depto['nombre']}."
-            except TimeoutError:
-                if pagina.url.endswith("/login") or "login" in pagina.title().lower():
-                    mensaje = f"Credenciales incorrectas para {depto['nombre']}."
-                else:
-                    mensaje = f"No se encontró la IP. Timeout en {depto['nombre']}."
-
-            enviar_mensaje_a_slack(mensaje)
-            pagina.wait_for_timeout(3000)
-        finally:
-            navegador.close()
 
 
+if __name__ == "__main__":
+    main()
