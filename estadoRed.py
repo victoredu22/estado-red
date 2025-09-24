af4333e7df1adaad7e15a4436eaa7d0f57a40280
victor <victor.edu22@gmail.com>
2025-09-24 01:20:29 -0300

    fix

--- estadoRed.py
+++ estadoRed.py
@@ -1,28 +1,11 @@
 import ssl
-import certifi
 import socketio
-import os
-from dotenv import load_dotenv
 
-# Cargar variables de entorno
-load_dotenv()
-sio_url = os.getenv("SERVER_SOCKET")
+sio = socketio.Client()
 
-# Crear cliente socket.io
-sio = socketio.Client(
-    reconnection=True,           # reintentos automáticos
-    reconnection_attempts=5,     # número de intentos
-    reconnection_delay=2         # segundos entre intentos
-)
-
-# Crear contexto SSL seguro con certifi
-ssl_context = ssl.create_default_context(cafile=certifi.where())
-
-# Eventos básicos
 @sio.event
 def connect():
-    print("✅ Conectado al servidor:", sio_url)
-    sio.emit("test", "Hola desde Python 🚀")
+    print("✅ Conectado al servidor")
 
 @sio.event
 def connect_error(data):
@@ -30,19 +13,14 @@ def connect_error(data):
 
 @sio.event
 def disconnect():
-    print("⚠️ Desconectado del servidor")
-
-# Ejemplo: escuchar un canal específico
-@sio.on("canalFrontend")
-def canal_frontend(data):
-    print("📩 Mensaje en canalFrontend:", data)
+    print("⚠️ Desconectado")
 
 def main():
     try:
         sio.connect(
-            sio_url,
+            "https://cumbresanramon.cl",     # URL en seco
             transports=["websocket"],
-            socketio_path="/socket.io"             # asegura validación SS          # usa el contexto con certifi
+            socketio_path="/socket.io"
         )
         sio.wait()
     except Exception as e:
